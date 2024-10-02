1c79b12d38fe939239c2e6b545f06016b43c467c
NehuenCovelo <nehuencovelo@gmail.com>
2024-10-02 16:56:01 -0300

    feat: create match-player relation

--- src/entities/match/index.ts
+++ src/entities/match/index.ts
@@ -1,1 +1,18 @@
-export class Match {}
+import { randomUUID, UUID } from "crypto";
+import { Player } from "../player";
+
+export class Match {
+  private _id: UUID;
+  player_1: Player;
+  player_2: Player;
+
+  constructor(match: { player_1: Player; player_2: Player }) {
+    this._id = randomUUID();
+    this.player_1 = match.player_1;
+    this.player_2 = match.player_2;
+  }
+
+  get id() {
+    return this._id;
+  }
+}
--- src/entities/player/index.ts
+++ src/entities/player/index.ts
@@ -1,1 +1,23 @@
-export class Player {}
+import { randomUUID, UUID } from "crypto";
+import { HandArea } from "./areas/hand-area";
+
+export class Player {
+  private _id: UUID;
+  hp: number;
+  playerHand: HandArea;
+
+  constructor() {
+    this._id = randomUUID();
+    this.hp = 20;
+    this.playerHand = new HandArea();
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  updateHp(update: number) {
+    this.hp += update;
+    return this.hp;
+  }
+}
