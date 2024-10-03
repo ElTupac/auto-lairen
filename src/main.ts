c7063fdc4ee461c40184eebac69035e4c43219bd
NehuenCovelo <nehuencovelo@gmail.com>
2024-10-03 16:12:31 -0300

    feat: start testing match creation

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
-console.log("");
+import { randomUUID } from "crypto";
+import { Deck } from "./entities/deck";
+import { Kingdom } from "./entities/deck/kingdom";
+import { Vault } from "./entities/deck/vault";
+import { Match } from "./entities/match";
+import { Player } from "./entities/player";
+
+const player_1 = new Player({
+  deck: new Deck({
+    kingdom: new Kingdom([]),
+    vault: new Vault([]),
+    player_id: randomUUID(),
+  }),
+});
+
+const player_2 = new Player({
+  deck: new Deck({
+    kingdom: new Kingdom([]),
+    vault: new Vault([]),
+    player_id: randomUUID(),
+  }),
+});
+
+new Match({
+  first_turn: "player_1",
+  player_1,
+  player_2,
+});
